f24c770a93ba5f470728de3d1258119267be2648
MrRocketR <kuptsovns@gmail.com>
2022-08-25 21:21:30 +0300

    4. Скачивание файла с ограничением

--- src/main/java/ru.job4j.concurrent/Wget.java
+++ src/main/java/ru.job4j.concurrent/Wget.java
@@ -1,21 +1,47 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    try {
-                        System.out.println("Start loading...");
-                        for (int i = 0; i <= 100; i++) {
-                            Thread.sleep(1000);
-                            System.out.print("\rLoading : " + i + "%");
-                        }
-                        System.out.println("\nLoaded!");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                if (bytesRead < 1024) {
+                    Thread.sleep(1000);
                 }
-        );
-        thread.start();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        if (url == null) {
+            throw new IllegalArgumentException("url is null");
+        }
+        if (speed == 0) {
+            throw new IllegalArgumentException("speed is zero");
+        }
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
     }
 }
