04b3fec0de62df5ee2127933ca5cab844b0a8525
MrRocketR <kuptsovns@gmail.com>
2022-08-25 20:45:59 +0300

    3. Прерывание нити

--- src/main/java/ru.job4j.concurrent/ConsoleProgress.java
+++ src/main/java/ru.job4j.concurrent/ConsoleProgress.java
@@ -1,13 +1,22 @@
 package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
-    char[] process = {'\\', '|', '/'};
     @Override
     public void run() {
+        char[] process = {'\\', '|', '/'};
         while (!Thread.currentThread().isInterrupted()) {
-            System.out.print("\r load: " + process[0]);
-            System.out.print("\r load: " + process[1]);
-            System.out.print("\r load: " + process[2]);
+            try {
+                System.out.print("\r load: " + process[0]);
+                Thread.sleep(100);
+                System.out.print("\r load: " + process[1]);
+                Thread.sleep(100);
+                System.out.print("\r load: " + process[2]);
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+
         }
     }
 }
--- src/main/java/ru.job4j.concurrent/Progress.java
+++ src/main/java/ru.job4j.concurrent/Progress.java
@@ -4,7 +4,7 @@ public class Progress {
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000);
+        Thread.sleep(10000);
         progress.interrupt();
     }
 }
