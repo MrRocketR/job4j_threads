3c9a9246d12de0add8b1cacf1129ecff0f1b5aba
MrRocketR <kuptsovns@gmail.com>
2022-09-06 18:41:19 +0300

    2. Обеспечить остановку потребителя

---
+++ src/main/java/ru/job4j/waitnotify/ParallelSearch.java
@@ -0,0 +1,29 @@
+package ru.job4j.waitnotify;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        ).start();
+    }
+}
