2dc73fc88752d4f7a5ac13fd6a5c0766189f679f
MrRocketR <kuptsovns@gmail.com>
2022-09-11 15:30:39 +0300

    2. Обеспечить остановку потребителя

--- src/main/java/ru/job4j/waitnotify/ParallelSearch.java
+++ src/main/java/ru/job4j/waitnotify/ParallelSearch.java
@@ -1,30 +1,30 @@
 package ru.job4j.waitnotify;
 
 public class ParallelSearch {
-
-    public static void main(String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
-        Thread producer  = new Thread(
-                () -> {
-                    for (int index = 0; index != 3; index++) {
-                        queue.offer(index);
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-
-        );
-        producer.start();
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
         final Thread consumer = new Thread(
                 () -> {
-                    while (producer.getState() == Thread.State.RUNNABLE) {
+                    while (!Thread.currentThread().isInterrupted()) {
                         System.out.println(queue.poll());
                     }
                 }
         );
         consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
     }
 }
