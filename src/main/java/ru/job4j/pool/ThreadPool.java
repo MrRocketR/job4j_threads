11dbdab0b0ebe878808efd9cab8c4f608305c7e2
MrRocketR <kuptsovns@gmail.com>
2022-09-13 23:04:33 +0300

    1. Реализовать ThreadPool

--- src/main/java/ru/job4j/pool/ThreadPool.java
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -7,11 +7,37 @@ import java.util.List;
 
 public class ThreadPool {
     private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+    int size = Runtime.getRuntime().availableProcessors();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
 
-    public void work(Runnable job) {
+
+    public void work(Runnable job) throws InterruptedException {
+        tasks.offer(job);
     }
 
     public void shutdown() {
+        for (Thread t : threads) {
+            t.interrupt();
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPool threadPool = new ThreadPool();
+        for (int i = 0; i < threadPool.size; i++) {
+           Thread t  =  (new Thread(() -> {
+               while (!Thread.currentThread().isInterrupted()) {
+                   System.out.println(Thread.currentThread().getName() + " " + Thread.currentThread().getState());
+               }
+           }
+           ));
+            threadPool.threads.add(t);
+            t.start();
+            threadPool.work(t);
+        }
+
+        Thread.sleep(10000);
+        threadPool.shutdown();
+
     }
 }
