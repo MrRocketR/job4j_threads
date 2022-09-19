93a43c54fc796e1fc50120578e9ae8ae367ef9b6
MrRocketR <kuptsovns@gmail.com>
2022-09-19 23:00:47 +0300

    ForkJoinPool

--- src/main/java/ru/job4j/pools/ParallelFindIndex.java
+++ src/main/java/ru/job4j/pools/ParallelFindIndex.java
@@ -4,28 +4,24 @@ import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
-public class ParallelFindIndex<T> extends RecursiveTask<T> {
+public class ParallelFindIndex  extends RecursiveTask<Integer> {
 
-    private final T[] array;
-    private final T required;
+    private final Integer[] array;
+    private final int start;
+    private final int end;
+    private final  Integer required;
 
-    public ParallelFindIndex(T[] array, T required) {
+
+    public ParallelFindIndex(Integer[] array,  int start, int end, Integer required) {
         this.array = array;
+        this.start = start;
+        this.end = end;
         this.required = required;
     }
 
-    private T findLinear(T[] array, T t) {
-        T[] arr = array;
-        for (int i = 0; i < arr.length - 1; i++) {
-             if (arr[i] == t) {
-                return t;
-            }
-        }
-        return null;
-    }
-    private T findWithPool(T[] array, int start, int finish, T t) {
-        for (int i = start; i < finish; i++) {
-            if (array[i] == t) {
+    private Integer findLinear(Integer[] array,  Integer t) {
+        for (int i = start; i < end; i++) {
+             if (array[i].equals(t)) {
                 return t;
             }
         }
@@ -34,26 +30,21 @@ public class ParallelFindIndex<T> extends RecursiveTask<T> {
 
 
     @Override
-    protected T compute() {
+    protected  Integer compute() {
         if (array.length <= 10) {
             return findLinear(array, required);
         }
-        ParallelFindIndex<T> left = new  ParallelFindIndex<>(Arrays.copyOfRange(array, 0, array.length / 2), required);
-        ParallelFindIndex<T> right = new  ParallelFindIndex<>(Arrays.copyOfRange(array, array.length / 2, array.length), required);
-        left.fork();
-        right.fork();
-        T findLeft = left.join();
-        T findRight = right.join();
-        return null;
+        ParallelFindIndex first = new ParallelFindIndex(array, start, end, required);
+        first.fork();
+        return first.findLinear(array, required);
     }
 
     public static void main(String[] args) {
-        Integer[] array = new Integer[] {1, 11, 45, 11, 65, 77, 88,
+       Integer[] array = new Integer[] {1, 11, 45, 11, 65, 77, 88,
                 22, 54, 100, 145, 123, 77,
                 88, 222, 500, 700, 666, 777, 888};
-        Integer required = 500;
-        ParallelFindIndex parallelFindIndex = new ParallelFindIndex(array, required);
+        ParallelFindIndex parallelFindIndex = new ParallelFindIndex(array, 0, 21, 500);
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        forkJoinPool.invoke(parallelFindIndex);
+        System.out.println(forkJoinPool.invoke(parallelFindIndex));
     }
 }
