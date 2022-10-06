e2b49da7b2a3d46ab777ff695915b1912d6b4390
MrRocketR <kuptsovns@gmail.com>
2022-10-06 17:13:11 +0300

    CompletableFuture

--- src/main/java/ru/job4j/async/RolColSum.java
+++ src/main/java/ru/job4j/async/RolColSum.java
@@ -4,11 +4,45 @@ public class RolColSum {
     public static class Sums {
         private int rowSum;
         private int colSum;
-        /* Getter and Setter */
-    }
 
-    public static Sums[] sum(int[][] matrix) {
-        return null;
+        public Sums(int rowSum, int colSum) {
+            this.rowSum = rowSum;
+            this.colSum = colSum;
+        }
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+
+        private static Sums linearSum(int[][] matrix, int row) {
+            int rows = 0;
+            int cols = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                rows += matrix[row][i];
+                cols += matrix[i][row];
+            }
+            return new Sums(rows, cols);
+        }
+
+        public static Sums[] sum(int[][] matrix) {
+            Sums[] rsl = new Sums[matrix.length];
+            for (int row = 0; row < matrix.length; row++) {
+                rsl[row] = linearSum(matrix, row);
+            }
+            return rsl;
+        }
     }
 
     public static Sums[] asyncSum(int[][] matrix) {
